2d0f5cc411dfdb28c4dc848b49c97dcc93718d82
ilker Yılmaz <ilkoylmz@gmail.com>
2020-10-19 23:36:08 +0300

    Add stylelint and update basic style system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement } from 'react';
 import type { AppProps } from 'next/app';
 
+import 'styles/styles.scss';
+
 function App({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
 }
